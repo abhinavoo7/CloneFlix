7ef77a962c42ec3197e32f2c2174d75259344989
Abhinav Gupta <gabhinav95a@gmail.com>
2021-11-19 16:33:29 +0530

    displaying video

--- watch.php
+++ watch.php
@@ -10,5 +10,10 @@
 
     $video = new Video($con, $entityId);
     $video->incrementViews();
-
 ?>
+
+<div class="watchContainer">
+    <video controls autoplay>
+        <source src="<?php echo $video->getFilePath(); ?>" type="video/mp4">
+    </video>
+</div>
